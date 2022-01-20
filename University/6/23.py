833f5fe59deb8958e5660ae458590a682b1b6bc3
Hubert <s223708@student.uek.krakow.pl>
2022-01-20 22:00:17 +0100

    nocka z redditem

--- University/6/23.py
+++ University/6/23.py
@@ -1,5 +1,8 @@
 def median(array):
     if len(array)%2==0:
-        return (array[(len(array)/2)-1] + array[len(array)/2])/2
+        return ((array[(len(array)/2)-1] + array[len(array)/2])/2)
     else:
-        return array[(len(array)-1)/2]
+        return array[int((len(array)-1)/2)]
+    
+print(median([1,0,9,4,6]))
+print(median([6,8,3,1,0,5,7]))
---
+++ University/6/24.py
@@ -0,0 +1,7 @@
+def function(array):
+    x = int(input("Enter number: "))
+    
+    for i in array:
+        if i>x: print(i)
+        
+function([6,8,3,1,0,5,7])
